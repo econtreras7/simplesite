dc43308ffdf08cfdff0a4319795168522d415c94
econtreras7 <econtreras7@ucmerced.edu>
2022-01-20 20:32:56 -0600

    updated order of heroku settings

--- simpleproject/settings.py
+++ simpleproject/settings.py
@@ -12,10 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 from decouple import config
-import django_heroku
 
-
-django_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -126,3 +123,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+import django_heroku
+
+
+django_heroku.settings(locals())
