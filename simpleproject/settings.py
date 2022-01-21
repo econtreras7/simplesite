cd935137f309e3c434ee56418b3d5e4f198b99b3
econtreras7 <econtreras7@ucmerced.edu>
2022-01-20 20:42:05 -0600

    Added a Debug env variable

--- simpleproject/settings.py
+++ simpleproject/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+from ctypes import cast
 from pathlib import Path
 from decouple import config
 
@@ -25,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG',cast=bool)
 
 ALLOWED_HOSTS = ['0.0.0.0']
 
