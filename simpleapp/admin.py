10526650c46e8915e84a5c2e7a1ac11a1970186d
econtreras7 <econtreras7@ucmerced.edu>
2022-01-23 23:00:19 -0600

    registering models in admin.py

--- simpleapp/admin.py
+++ simpleapp/admin.py
@@ -1,3 +1,17 @@
+from pyexpat import model
 from django.contrib import admin
 
+from simpleapp.models import Business
+
 # Register your models here.
+
+
+class BusinessInLine(admin.TabularInline):
+    model = Business
+    
+class BusinessAdmin(admin.ModelAdmin):
+    inlines =[
+        BusinessInLine
+    ]
+
+admin.site.register(Business,BusinessAdmin)
